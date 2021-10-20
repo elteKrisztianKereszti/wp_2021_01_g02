0c1e291aeb361494aa5ea6892a181cb3ad1535b5
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2021-10-20 14:07:25 +0200

    practice 7.

--- p07/index.js
+++ p07/index.js
@@ -1,32 +1,36 @@
-let randombtn = document.querySelector('#randombtn') ;
+let query = document.querySelector('#query') ;
+let getWeatherInfoBtn = document.querySelector('#getWeatherInfoBtn') ;
 let output = document.querySelector('#output') ;
 
-randombtn.addEventListener('click', async () => {
-  getRandomJoke();
-  let randomJoke = await getRandomJokeWithFetch();
-  console.log(randomJoke);
-  output.innerHTML = 'fetch: ' + randomJoke;
+getWeatherInfoBtn.addEventListener('click', async () => {
+  let geoData = await getGeodata(query.value);
+  if (!geoData || geoData.features.length == 0) {
+    output.innerHTML = 'NO DATA';
+    return;
+  }
+  
+  let coordinates = processGeoData(geoData);
+  let wet = await getWetData(coordinates);
+  console.log(wet);
+  output.innerHTML = 'DATA: ' + JSON.stringify(wet);
 });
 
-function getRandomJoke() {
-  const xhr = new XMLHttpRequest();
-
-  xhr.open('GET', 'http://api.icndb.com/jokes/random');
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', getRandomJokeHandler)
-  xhr.send(null);
-}
-
-function getRandomJokeHandler() {
-  output.innerHTML = 'xml: ' +  this.response.value.joke;
-}
-
-async function getRandomJokeWithFetch() {
-  // fetch('http://api.icndb.com/jokes/random')
-  //   .then(response => response.json())
-  //   .then(response => { return response });
-  const response = await fetch('http://api.icndb.com/jokes/random');
+async function getGeodata(query) {
+  const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${query}&format=geojson`);
   const json = await response.json();
-  return json.value.joke;
+  return json;
 }
+
+function processGeoData(geoData) {
+  return geoData.features[0].geometry.coordinates;
+}
+
+async function getWetData(coordinates) {
+  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${coordinates[0]}&lon=${coordinates[1]}&appid=8607a0d84d57d8ae4f7ca3c71200e60e `);
+  const json = await response.json();
+  return json;
+}
+
+
+
 
